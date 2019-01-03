883ace60b4f31f1579f54439d471bac26e6348d9
Shefali Agarwal <shefali.agarwal251@gmail.com>
2019-01-02 21:22:49 -0500

    Backend: Mongoose validation

--- backend/models/conversionLog.js
+++ backend/models/conversionLog.js
@@ -2,12 +2,24 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model('ConversionLog', new mongoose.Schema({
   fromCurrency: {
-    name: String,
-    amount: Number
+    name: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    }
   },
   toCurrency: {
-    name: String,
-    amount: Number
+    name: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    }
   },
   conversionRate: Number,
   date: Date
